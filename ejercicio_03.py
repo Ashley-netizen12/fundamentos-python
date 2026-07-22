522fb4ec0517406b2d9956109be15a62dec18531
Ashley-netizen12 <mahomagacha@gmail.com>
2026-07-22 10:33:35 -0400

    se añadio lista de zapatillas

--- ejercicio_03.py
+++ ejercicio_03.py
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get ("/zapatillas")
+async def obtenerzapatillas ():
+    return [
+        {"id": 1, "marca": "Nike", "talla": 40-50},
+        {"id": 2, "marca": "Addidas", "talla": 30-35},
+        {"id": 3, "marca": "Nike", "talla": 35-45}
+    ]
